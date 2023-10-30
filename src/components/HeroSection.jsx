3fafdf63f6d65fcf84301b5bbf52e589e453e2d6
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-10-30 20:31:12 +0530

    hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../style/HeroSection.css';
 
 function HeroSection() {
+    
+    useEffect(() => {
+        // Load the Spline script dynamically
+        const script = document.createElement('script');
+        script.src = 'https://unpkg.com/@splinetool/viewer@0.9.490/build/spline-viewer.js';
+        script.type = 'module';
+        document.body.appendChild(script);
+
+        // Clean up on component unmount
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
     return (
-        <div className="hero-section">
-            <h1>Welcome to nxtep</h1>
-            <p>Discover our services and offerings.</p>
-            <button className="cta-button">Learn More</button>
+        <div >
+            <spline-viewer url="https://prod.spline.design/aYX6mk5TVfwhdIRX/scene.splinecode"></spline-viewer>
         </div>
     );
 }
