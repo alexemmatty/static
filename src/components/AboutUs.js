feaec7d32028ff260dd32680ac217bc7dce09a74
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-11-05 21:03:54 +0530

    me

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,44 +1,42 @@
 import React from 'react';
-import { Container, Row, Col, Tab, Nav } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
-import { Services } from './Service';
 import '../styles/about.css';
-import image from '../assets/img/bd.png';
-const AboutSectionHeader = ({ isVisible }) => (
-  <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-    <h2>About Us</h2>
-    <p>This is a place where we share our company story and values. Learn more about our mission, vision, and the values that drive us.</p>
-  </div>
-);
+import creativeImage from '../assets/img/bd.png';
 
-const AboutTabContent = ({ eventKey, isVisible, children }) => (
-  <Tab.Pane eventKey={eventKey}>
-    <div className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-      {children}
-    </div>
-  </Tab.Pane>
-);
+// Import your Services component
+import { Services } from './Service'; // Assuming the default export is used
 
-function About () {
+function About() {
   return (
-    <section className="about-section" id="about">
-      <Container>
-        <Services />
-        <Row className="justify-content-center text-center">
-        <div className="d-flex flex-row justify-content-center align-items-center">
-        <div className="p-2">
-          <img src={image} className="img-fluid" alt="Responsive image" />
-        </div>
-        <div className="p-2">
-          <h1>Your Title Text Here</h1>
-          <p>Your description or other content here...</p>
-        </div>
-      </div>
-        </Row>
-      </Container>
-    </section>
+    <>
+      <Services />
+      <section className="about-section" id="about">
+        <Container>
+          <TrackVisibility once partialVisibility>
+            {({ isVisible }) => (
+              <Row className="align-items-center">
+                <Col lg={6} md={12} className="text-part">
+                  <h1>We're <span className="highlight">Scoda Agency</span></h1>
+                  <p className="description">
+                    We are a fully in-house digital agency focusing on branding, marketing, web design and development with clients ranging from start-ups. We pride ourselves on partnering with clients in order to give the most transparent and educational experience.
+                  </p>
+                </Col>
+                <Col lg={6} md={12} className="image-part d-none d-lg-block">
+                  <img
+                    src={creativeImage}
+                    alt="Creative Space"
+                    className={`img-fluid ${isVisible ? 'animate__animated animate__zoomIn' : ''}`}
+                  />
+                </Col>
+              </Row>
+            )}
+          </TrackVisibility>
+        </Container>
+      </section>
+    </>
   );
-};
+}
 
 export default About;
