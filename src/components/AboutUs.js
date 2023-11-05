52bb2ee4a58ef4d885baedb160c00b1adea7e669
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-11-05 12:22:00 +0530

    about 3

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -10,54 +10,11 @@ export const About = () => {
       <Container>
         <Service />
       <Row className="justify-content-center text-center">
-          <Col lg={8} md={10}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                  <h2>Who We Are</h2>
-                  <p className="mt-3">Our company is built on the foundation of innovation, integrity, and impact. With a team that values collaboration and creativity, we've been able to pioneer solutions that truly make a difference.</p>
-                </div>
-              }
-            </TrackVisibility>
-          </Col>
-        </Row>
-        <Row className="mt-5">
-          <Col lg={4} md={6} className="text-center">
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeInLeft" : ""}>
-                  <h3>Our Mission</h3>
-                  <p>To deliver exceptional value through innovative products and services that empower our clients and enrich our communities.</p>
-                </div>
-              }
-            </TrackVisibility>
-          </Col>
-          <Col lg={4} md={6} className="text-center">
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeInUp" : ""}>
-                  <h3>Our Vision</h3>
-                  <p>To be the leader in our industry, setting standards that others follow, driving forward to new horizons and achievements.</p>
-                </div>
-              }
-            </TrackVisibility>
-          </Col>
-          <Col lg={4} md={6} className="text-center">
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeInRight" : ""}>
-                  <h3>Our Values</h3>
-                  <p>Excellence, collaboration, and innovation. We believe these pillars are the key to making a positive impact in the world.</p>
-                </div>
-              }
-            </TrackVisibility>
-          </Col>
-        </Row>
+        <h2>asaas</h2>
+      </Row>
       </Container>
 
-      {/* <Container>
-        <Row className="justify-content-center text-center">
-          <Col lg={8} md={10}>
+      {/*<Col lg={8} md={10}>
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
@@ -99,8 +56,7 @@ export const About = () => {
               }
             </TrackVisibility>
           </Col>
-        </Row>
-      </Container> */}
+        </Row> */}
     </section>
   )
 }
