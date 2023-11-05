95f5d2c36c2c907f6a9b1eccad3919cf137d5f74
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-11-05 13:03:38 +0530

    seo

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,62 +1,42 @@
-import { Container, Row, Col } from "react-bootstrap";
+import React from 'react';
+import { Container, Row, Col, Tab, Nav } from 'react-bootstrap';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
-import Service from './Service.js';
+import Service from './Service';
+import '../styles/about.css';
+import image from '../assets/img/bd.png';
+const AboutSectionHeader = ({ isVisible }) => (
+  <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+    <h2>About Us</h2>
+    <p>This is a place where we share our company story and values. Learn more about our mission, vision, and the values that drive us.</p>
+  </div>
+);
+
+const AboutTabContent = ({ eventKey, isVisible, children }) => (
+  <Tab.Pane eventKey={eventKey}>
+    <div className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+      {children}
+    </div>
+  </Tab.Pane>
+);
 
 export const About = () => {
   return (
-    <section className="about" id="about">
-      
+    <section className="about-section" id="about">
       <Container>
         <Service />
-      <Row className="justify-content-center text-center">
-        <h2>asaas</h2>
-      </Row>
-      </Container>
-
-      {/*<Col lg={8} md={10}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                  <h2>Who We Are</h2>
-                  <p className="mt-3">Our company is built on the foundation of innovation, integrity, and impact. With a team that values collaboration and creativity, we've been able to pioneer solutions that truly make a difference.</p>
-                </div>
-              }
-            </TrackVisibility>
-          </Col>
+        <Row className="justify-content-center text-center">
+        <div className="d-flex flex-row justify-content-center align-items-center">
+        <div className="p-2">
+          <img src={image} className="img-fluid" alt="Responsive image" />
+        </div>
+        <div className="p-2">
+          <h1>Your Title Text Here</h1>
+          <p>Your description or other content here...</p>
+        </div>
+      </div>
         </Row>
-        <Row className="mt-5">
-          <Col lg={4} md={6} className="text-center">
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeInLeft" : ""}>
-                  <h3>Our Mission</h3>
-                  <p>To deliver exceptional value through innovative products and services that empower our clients and enrich our communities.</p>
-                </div>
-              }
-            </TrackVisibility>
-          </Col>
-          <Col lg={4} md={6} className="text-center">
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeInUp" : ""}>
-                  <h3>Our Vision</h3>
-                  <p>To be the leader in our industry, setting standards that others follow, driving forward to new horizons and achievements.</p>
-                </div>
-              }
-            </TrackVisibility>
-          </Col>
-          <Col lg={4} md={6} className="text-center">
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeInRight" : ""}>
-                  <h3>Our Values</h3>
-                  <p>Excellence, collaboration, and innovation. We believe these pillars are the key to making a positive impact in the world.</p>
-                </div>
-              }
-            </TrackVisibility>
-          </Col>
-        </Row> */}
+      </Container>
     </section>
-  )
-}
+  );
+};
