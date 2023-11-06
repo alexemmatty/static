345ee61f48536567289953570964924634d1d7a6
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-11-06 15:24:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ console.log(process.env.EMAIL_PASS);
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: "nxtep.in@gmail.com",
+    pass: "toinfinityandbeyond"
   },
 });
 
