f492d64c4fa0ce61df917089e37a7afe008c84a6
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-11-07 12:22:47 +0530

    contact set

--- src/App.js
+++ src/App.js
@@ -8,6 +8,23 @@ import  Footer  from "./components/Footer";
 import About  from './components/AboutUs';
 import React from "react";
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsiCMj43nOKqc-6MERyBoi5AqarKaRmrI",
+  authDomain: "nxtep-contact.firebaseapp.com",
+  projectId: "nxtep-contact",
+  storageBucket: "nxtep-contact.appspot.com",
+  messagingSenderId: "562902432010",
+  appId: "1:562902432010:web:03353522c825365ae0a2f3",
+  measurementId: "G-6P19YRZTBN"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 function App() {
   return (
     <div className="App">
