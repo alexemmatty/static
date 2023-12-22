5a8d77b13597eefff23aea0a6df98e7668cfd7b3
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-12-22 21:31:20 +0530

    footer fixed @YoungGuy2003

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,27 @@
 import { Container, Row, Col } from "react-bootstrap";
-// import logo from "../assets/img/logo.svg";
-// import navIcon1 from "../assets/img/nav-icon1.svg";
-// import navIcon2 from "../assets/img/nav-icon2.svg";
-// import navIcon3 from "../assets/img/nav-icon3.svg";
+
 import "../styles/footer.css";
 
 function Footer() {
   return (
     <footer className="footer">
       <Container>
-        <Row className="align-items-center justify-content-center text-center">
-          <Col xs={12} sm={6} md={4} className="mb-3 mb-sm-0">
-            <h4>Contact Us</h4>
-            <p>Phone: +91 8289819891</p>
-          </Col>
-          <Col xs={12} sm={6} md={4} className="mb-3 mb-sm-0">
-            {/* Replace "Visit Us" with a logo */}
+        <Row className="align-items-center justify-content-between">
+          <Col xs={12} md={6} className="mb-3 mb-md-0">
             <h2 className="brand-name">n<span>X</span>tep</h2>
           </Col>
-          <Col xs={12} sm={12} md={4} className="mb-3 mb-sm-0">
-            <h4>Mail Us</h4>
-            <p>nxtep.in@gmail.com</p>
+          <Col xs={12} md={6} className="text-md-right mb-3 mb-md-0">
+            <div className="footer-nav">
+              <a href="#home">Home</a>
+              <a href="#about">About Us</a>
+              <a href="#Service">Our Services</a>
+              <a href="#contact">Contact Us</a>
+            </div>
           </Col>
         </Row>
-        <Row className="align-items-center justify-content-center text-center">
-          <Col xs={12} sm={12} className="mb-3 mb-sm-0">
+        <hr />
+        <Row className="text-center">
+          <Col>
             <p>Copyright © {new Date().getFullYear()}. All Rights Reserved</p>
           </Col>
         </Row>
